083547153f195a77386a7855e079d314e0c912c3
tawheedshimul <tawheedshimul@gmail.com>
2023-11-23 15:05:25 +0600

    update

--- src/Pages/Setting.jsx
+++ src/Pages/Setting.jsx
@@ -3,9 +3,9 @@ import { NumericFormat } from 'react-number-format';
 
 const Setting = () => {
     const [products, setProducts] = useState([
-        { name: 'Grolsch', size: '50l Keg', yield: 300, netPrice: 150, unitPrice: 0.5, salesPriceVAT: 4, salesPriceLessVAT: 3.333333333, gpNetPrice: '85.00' },
-        { name: 'Meantim', size: '30L keg', yield: 88, netPrice: 150, unitPrice: 1.704545455, salesPriceVAT: 4, salesPriceLessVAT: 3.333333333, gpNetPrice: '49.00' },
-        { name: 'Peroni', size: '50l Keg', yield: 88, netPrice: 150, unitPrice: 1.704545455, salesPriceVAT: 4, salesPriceLessVAT: 3.333333333, gpNetPrice: '49.00' },
+        { name: 'Grolsch', size: '50l Keg', yield: 300, netPrice: 150, unitPrice: 0.5, salesPriceVAT: 4, salesPriceLessVAT: 3.33, gpNetPrice: '85.00' },
+        { name: 'Meantim', size: '30L keg', yield: 88, netPrice: 150, unitPrice: 1.71, salesPriceVAT: 4, salesPriceLessVAT: 3.33, gpNetPrice: '49.00' },
+        { name: 'Peroni', size: '50l Keg', yield: 88, netPrice: 150, unitPrice: 1.71, salesPriceVAT: 4, salesPriceLessVAT: 3.33, gpNetPrice: '49.00' },
     ]);
 
     const VAT_RATE = 1.2;
@@ -21,23 +21,22 @@ const Setting = () => {
         if (field === 'netPrice' || field === 'yield') {
             if (field === 'netPrice') {
                 // Handle netPrice
-                const unitPrice = value / updatedProducts[index].yield;
+                const unitPrice = Number(value) / Number(updatedProducts[index].yield);
                 updatedProducts[index].unitPrice = unitPrice.toFixed(2);
             } else if (field === 'yield') {
                 // Handle yield
-                updatedProducts[index].yield = value;
-                const unitPrice = updatedProducts[index].netPrice / value;
+                updatedProducts[index].yield = Number(value);
+                const unitPrice = Number(updatedProducts[index].netPrice) / Number(value);
                 updatedProducts[index].unitPrice = unitPrice.toFixed(2);
             }
         } else if (field === 'salesPriceVAT') {
             // Handle salesPriceVAT
-            updatedProducts[index].salesPriceLessVAT = (value / VAT_RATE).toFixed(2);
+            updatedProducts[index].salesPriceLessVAT = (Number(value) / VAT_RATE).toFixed(2);
         } else {
             // Handle other cases
-            const unitPrice = value / updatedProducts[index][field];
+            const unitPrice = Number(value) / Number(updatedProducts[index][field]);
             updatedProducts[index].unitPrice = unitPrice.toFixed(2);
         }
-
 
         // Calculate GP NET PRICE %
         updatedProducts[index].gpNetPrice = calculateGpNetPrice(
@@ -51,16 +50,14 @@ const Setting = () => {
     const handleSizeChange = (index, value) => {
         const updatedProducts = [...products];
         updatedProducts[index].size = value;
-
         setProducts(updatedProducts);
     };
 
     return (
         <div className="container mx-auto p-4">
-            <h2 className='font-bold text-2xl text-center  p-2 border'>Draguht - Alc</h2>
+            <h2 className='font-bold text-2xl text-center  p-2 border'>Draft - Alc</h2>
             <table className="min-w-full border border-gray-300">
                 <thead>
-
                     <tr>
                         <th className="border-b">Name</th>
                         <th className="border-b">Size</th>
@@ -77,8 +74,7 @@ const Setting = () => {
                         <tr key={index} className="text-center">
                             <td className="border">{product.name}</td>
                             <td className="border">
-
-                                <NumericFormat className="p-1" value="30" allowLeadingZeros thousandSeparator="," />
+                                <NumericFormat className="p-1" value={product.size} allowLeadingZeros thousandSeparator="," />
                             </td>
                             <td className="border">
                                 <NumericFormat
@@ -94,6 +90,7 @@ const Setting = () => {
                                     type="number"
                                     value={product.netPrice}
                                     onChange={(e) => handleInputChange(index, 'netPrice', e.target.value)}
+                                    decimalScale={2}
                                 />
                             </td>
                             <td className="border">{`£${product.unitPrice}`}</td>
@@ -105,10 +102,10 @@ const Setting = () => {
                                     onChange={(e) => handleInputChange(index, 'salesPriceVAT', e.target.value)}
                                     inputMode="numeric"
                                     style={{ appearance: "textfield" }}
+                                    decimalScale={2}
                                 />
                             </td>
-                            <td className="border">{product.salesPriceLessVAT}</td>
-
+                            <td className="border">{`£${product.salesPriceLessVAT}`}</td>
                             <td className="border">{`${product.gpNetPrice}%`}</td>
                         </tr>
                     ))}
@@ -119,3 +116,4 @@ const Setting = () => {
 };
 
 export default Setting;
+
